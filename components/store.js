dba0947d80516916fbdc76159d2c2d2dd8a9287e
isfandkhan <isfandyar_khan@live.co.uk>
2021-03-31 22:31:25 +0500

    added API caching mechanism

--- components/store.js
+++ components/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 import thunk from 'redux-thunk';
+import cacheData from 'memory-cache';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const debug = process.env.NEXT_PUBLIC_REDUX_DEBUG;
@@ -16,11 +17,24 @@ const reducer = (state = { posts: [] }, action) => {
   }
 };
 
+async function callAPI(url) {
+  const value = cacheData.get(url);
+  if (value) {
+    return new Promise((resolve) => {
+      resolve(value);
+    });
+  } else {
+    return fetch(url).then((res) => {
+      const hours = 24;
+      const data = res.json();
+      cacheData.put(url, data, hours * 1000 * 60 * 60);
+      return data;
+    });
+  }
+}
+
 export function fetchPosts() {
-  return (dispatch) =>
-    fetch(`${API_URL}/posts`)
-      .then((res) => res.json())
-      .then((posts) => dispatch({ type: 'SET_POSTS', posts }));
+  return (dispatch) => callAPI(`${API_URL}/posts`).then((posts) => dispatch({ type: 'SET_POSTS', posts }));
 }
 
 const makeStore = (context) => createStore(reducer, applyMiddleware(thunk));
