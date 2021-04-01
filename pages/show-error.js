4b9068066629f914aa44e9165f034e092fa08cbe
isfandkhan <isfandyar_khan@live.co.uk>
2021-04-02 04:26:24 +0500

    added sentry support

--- pages/show-error.js
+++ pages/show-error.js
@@ -1,7 +1,12 @@
-
-
-const CustomErrorGenerator = () => {
-    throw new Error();
-}
+const CustomErrorGenerator = () => (
+  <button
+    className="btn btn-danger"
+    onClick={() => {
+      throw new Error();
+    }}
+  >
+    Click
+  </button>
+);
 
 export default CustomErrorGenerator;
