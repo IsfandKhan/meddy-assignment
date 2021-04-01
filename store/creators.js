7fdbca624b93a04d048b7796d0517b2b4d20adb1
isfandkhan <isfandyar_khan@live.co.uk>
2021-04-01 19:07:09 +0500

    error boundary for single post page added

--- store/creators.js
+++ store/creators.js
@@ -1,5 +1,5 @@
 import { callAPI } from './api-cache';
-import { SET_POST, SET_POSTS  } from "./action";
+import { SET_POST, SET_POSTS } from './action';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -7,4 +7,9 @@ export const fetchPosts = () => (dispatch) =>
   callAPI(`${API_URL}/posts`).then((posts) => dispatch({ type: SET_POSTS, posts }));
 
 export const fetchPost = (postId) => (dispatch) =>
-  callAPI(`${API_URL}/posts/${postId}`).then((post) => dispatch({ type: SET_POST, post }));
+  callAPI(`${API_URL}/posts/${postId}`).then((post) => {
+    if (post && Object.keys(post).length === 0 && post.constructor === Object) {
+      throw new Error({ statusCode: 404 });
+    }
+    dispatch({ type: SET_POST, post });
+  });
