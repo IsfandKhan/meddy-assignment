0f383993f9f83f4d6201e3fc64f70de17985d057
isfandkhan <isfandyar_khan@live.co.uk>
2021-04-02 22:17:13 +0500

    added support for error logging on API failure

--- store/creators.js
+++ store/creators.js
@@ -1,8 +1,19 @@
+import * as sentry from '@sentry/browser';
 import { request } from '../backend-services';
 import { SET_POST, SET_POSTS } from './action';
 
+const errorHandler = (err) => {
+  sentry.captureException(err);
+};
+
 export const fetchPosts = () => (dispatch) =>
-  request.get('/posts').then((posts) => dispatch({ type: SET_POSTS, posts }));
+  request
+    .get('/posts')
+    .then((posts) => dispatch({ type: SET_POSTS, posts }))
+    .catch(errorHandler);
 
 export const fetchPost = (postId) => (dispatch) =>
-  request.get(`/posts/${postId}`).then((post) => dispatch({ type: SET_POST, post }));
+  request
+    .get(`/posts/${postId}`)
+    .then((post) => dispatch({ type: SET_POST, post }))
+    .catch(errorHandler);
